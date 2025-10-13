fa639c875172b82529cf41d02b9f832590d0a2a9
MadiEsca <marcosvinny354@gmail.com>
2025-10-13 12:37:35 -0300

    Code: Integracao com o Django

--- FinYou/app_financeiro/urls.py
+++ FinYou/app_financeiro/urls.py
@@ -4,7 +4,7 @@ from .views import home, login, recuperarSenha, dashboard, pagamento, criarConta
 urlpatterns = [
     path("", home, name="rota_home"),
     path("login/", login, name="rota_login"),
-    path("criarConta/", criarConta, name="rota_dashboard"),
+    path("criarConta/", criarConta, name="rota_criarConta"),
     path("recuperarSenha/", recuperarSenha, name="rota_recuperarSenha"),
     path("dashboard/", dashboard, name="rota_dashboard"),
     path("pagamento/", pagamento, name="rota_pagamento")
