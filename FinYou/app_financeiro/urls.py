92d0c585b6c8def8a3135cb49a1689c0fe509ced
MadiEsca <marcosvinny354@gmail.com>
2025-10-13 10:41:31 -0300

    Code: Adicionando rotas padrao

--- FinYou/app_financeiro/urls.py
+++ FinYou/app_financeiro/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, login, recuperarSenha, dashboard, pagamento, criarConta
 
-urlpatterns = [path("", home, name="Home")]
+urlpatterns = [
+    path("", home, name="rota_home"),
+    path("login/", login, name="rota_login"),
+    path("criarConta/", criarConta, name="rota_dashboard"),
+    path("recuperarSenha/", recuperarSenha, name="rota_recuperarSenha"),
+    path("dashboard/", dashboard, name="rota_dashboard"),
+    path("pagamento/", pagamento, name="rota_pagamento")
+]
--- FinYou/app_financeiro/views.py
+++ FinYou/app_financeiro/views.py
@@ -5,3 +5,18 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "home.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def criarConta(request):
+    return render(request, "nova_conta.html")
+
+def recuperarSenha(request):
+    return render(request, "recuperacao.html")
+
+def dashboard(request):
+    return HttpResponse("<h1>Dashboard</h1>")
+
+def pagamento(request):
+    return HttpResponse("<h1>Página de pagamento</h1>")
