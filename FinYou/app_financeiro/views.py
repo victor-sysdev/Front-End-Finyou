61dda9fda322a354f60301c8934762ec14a45f49
MadiEsca <marcosvinny354@gmail.com>
2025-10-15 04:29:23 -0300

    Code: Criando validação dos usuarios

--- FinYou/app_financeiro/views.py
+++ FinYou/app_financeiro/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from django.views import generic
 from django.urls import reverse_lazy
+
+#Etapas de configuração de autenticação dos usuários
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User
+
 
 # Criando as Views
 
@@ -10,7 +14,10 @@ def home(request):
     return render(request, "app_financeiro/home.html")
 
 def login(request):
-    return render(request, "app_financeiro/login.html")
+    if request.method == "GET":
+        return render(request, "app_financeiro/login.html")
+    else:
+        return HttpResponse("Tá tirando")
 
 def criarConta(request):
     return render(request, "app_financeiro/nova_conta.html")
