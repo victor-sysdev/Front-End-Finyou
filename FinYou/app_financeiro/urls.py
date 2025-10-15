0d7da8ffb27da6bf3192a63ee942d6f780c4b19c
MadiEsca <marcosvinny354@gmail.com>
2025-10-15 17:51:24 -0300

    Code: Implementando a autenticacao e criacao de usuarios

--- FinYou/app_financeiro/urls.py
+++ FinYou/app_financeiro/urls.py
@@ -1,11 +1,13 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
-from .views import home, login, recuperarSenha, dashboard, pagamento, criarConta
+from .views import home, login, recuperarSenha, teste, pagamento, criarConta
+from .forms import Formulario_Login
 
 urlpatterns = [
     path("", home, name="rota_home"),
-    path("login/", login, name="rota_login"),
+    path("login/", login, name="rota_login", ),
     path("criarConta/", criarConta, name="rota_criarConta"),
     path("recuperarSenha/", recuperarSenha, name="rota_recuperarSenha"),
-    path("dashboard/", dashboard, name="rota_dashboard"),
-    path("pagamento/", pagamento, name="rota_pagamento")
-]
+    path("teste/", teste, name="rota_teste"),
+    path("pagamento/", pagamento, name="rota_pagamento"),
+    ]
--- FinYou/app_financeiro/views.py
+++ FinYou/app_financeiro/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic
 from django.urls import reverse_lazy
@@ -7,6 +7,8 @@ from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.models import User
 
+#Importação de arquivos locais
+from .forms import Formulario_Login, Formulario_Criar_Conta
 
 # Criando as Views
 
@@ -15,17 +17,52 @@ def home(request):
 
 def login(request):
     if request.method == "GET":
-        return render(request, "app_financeiro/login.html")
+        return render(request, "app_financeiro/login.html", {"Formulario_Login": Formulario_Login()})
+    
     else:
-        return HttpResponse("Tá tirando")
+        form = Formulario_Login(request.POST) #Inicializa uma instancia do formulário com base nos dados passados pelo formulario
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            email = form.cleaned_data["email"] #NÃO UTILIZADA ATÉ O MOMENTO
+
+            #Verificando se o usuario existe
+            user = authenticate(username=username, password=password)
+            
+            if user:
+                # auth_login(request, user=user)
+                return HttpResponse(f"Pode fazer login - usuario {user} autenticado")
+            else:
+                return redirect()
+    
 
 def criarConta(request):
-    return render(request, "app_financeiro/nova_conta.html")
+    if request.method == "GET":
+        return render(request, "app_financeiro/criar_conta.html", {"Formulario_Criar_Conta":Formulario_Criar_Conta()})
+    else:
+        form = Formulario_Criar_Conta(request.POST) #Acessando os dados passados pelo meu formuçário
+        if form.is_valid(): #Verificando se o formulário foi preenchido corretamente
+            #pegando as informações do formulário
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            
+            #Verificando se o usuário já está cadastrado (com base no username e no email)
+            user = User.objects.filter(username=username, email=email).exists()
+            if user:
+                return HttpResponse(f"Usuario {username} já foi criado, entre em sua conta")
+            #Criando o Usuario
+            else:
+                print("Usuario criado agora")
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return HttpResponse(f"{username}, {email}, {password}")
+        
 
 def recuperarSenha(request):
     return render(request, "app_financeiro/recuperacao.html")
 
-def dashboard(request):
+def teste(request):
     return render(request, "teste.html")
 
 def pagamento(request):
